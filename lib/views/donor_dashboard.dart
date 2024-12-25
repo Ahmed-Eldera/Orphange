b9ded51502f87728a1b79ea3e6a348050f2f3cde
Yassin Khaled <yassinkhaled@Yassins-MacBook-Pro.local>
2024-12-25 16:41:55 +0200

    Updated Donor Dashboard View Page

--- lib/views/donor_dashboard.dart
+++ lib/views/donor_dashboard.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class DonorDashboard extends StatelessWidget {
-  const DonorDashboard({Key? key}) : super(key: key);
+  const DonorDashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -33,27 +33,65 @@ class DonorDashboard extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        color: Colors.white,
-        padding: const EdgeInsets.all(16.0),
-        child: GridView.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: 16.0,
-          mainAxisSpacing: 16.0,
-          childAspectRatio: 1,
-          children: [
-            _buildDashboardButton('View Events', () {}),
-            _buildDashboardButton('View Logs', () {}),
-            _buildDashboardButton('Register a Ticket', () {}),
-            _buildDashboardButton('Make a Donation', () {}),
-            _buildDashboardButton('Generate Receipts', () {}),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16.0,
+                mainAxisSpacing: 16.0,
+                childAspectRatio: 1,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  _buildDashboardButton(
+                    'View Events',
+                    Icons.event,
+                        () {},
+                  ),
+                  _buildDashboardButton(
+                    'Register a Ticket',
+                    Icons.confirmation_num,
+                        () {},
+                  ),
+                  _buildDashboardButton(
+                    'Donate',
+                    Icons.volunteer_activism,
+                        () {},
+                  ),
+                  _buildDashboardButton(
+                    'Generate Receipts',
+                    Icons.receipt,
+                        () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+        },
+        backgroundColor: Colors.blue.shade700,
+        icon: const Icon(
+          Icons.list_alt,
+          color: Colors.white,
+        ),
+        label: const Text(
+          'Logs',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildDashboardButton(String label, VoidCallback onPressed) {
+  Widget _buildDashboardButton(String label, IconData icon, VoidCallback onPressed) {
     return ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
@@ -62,22 +100,33 @@ class DonorDashboard extends StatelessWidget {
           borderRadius: BorderRadius.circular(8.0),
         ),
       ),
-      child: Text(
-        label,
-        style: const TextStyle(
-          color: Colors.white,
-          fontSize: 18,
-          fontFamily: 'Roboto',
-          fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+            size: 36,
+          ),
+          const SizedBox(height: 8),
+          Text(
+            label,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
       ),
     );
   }
 }
 
 class ProfilePlaceholderPage extends StatelessWidget {
-  const ProfilePlaceholderPage({Key? key}) : super(key: key);
+  const ProfilePlaceholderPage({super.key});
 
   @override
   Widget build(BuildContext context) {
